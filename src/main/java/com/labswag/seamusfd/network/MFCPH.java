7ec2085723b19d0e07a0221668c13d4ee7e1a09b
SeamusFD <armstrongmediahome@icloud.com>
2014-06-21 11:16:22 -0500

    Fixed issues... The UniversalCore integration is now complete

--- src/main/java/com/labswag/seamusfd/network/MFCPH.java
+++ src/main/java/com/labswag/seamusfd/network/MFCPH.java
@@ -2,13 +2,13 @@ package com.labswag.seamusfd.network;
 
 import codechicken.lib.packet.PacketCustom;
 import codechicken.lib.packet.PacketCustom.IServerPacketHandler;
+import com.labswag.seamusfd.libs.ModInfo;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.INetHandlerPlayServer;
-import universalteam.minefall.libs.ModReference;
 
 public class MFCPH implements IServerPacketHandler
 {
-	public static final String channel = ModReference.CHANNEL;
+	public static final String channel = ModInfo.CHANNEL;
 
 	@Override
 	public void handlePacket(PacketCustom packet, EntityPlayerMP player, INetHandlerPlayServer netHandler)
