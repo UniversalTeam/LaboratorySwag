7649e1348d31027a42c24a1a1b7307b46fe529cf
SeamusFD <armstrongmediahome@icloud.com>
2014-06-23 11:41:20 -0500

    Added some changes for @BigXplosion to look at

--- src/main/java/com/labswag/seamusfd/blocks/machines/MachineScientificGrinder.java
+++ src/main/java/com/labswag/seamusfd/blocks/machines/MachineScientificGrinder.java
@@ -48,10 +48,10 @@ public class MachineScientificGrinder extends BlockContainer {
     }
 
 
-    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
-    {
-        return Item.getItemFromBlock(CommonProxy.machineScientificGrinder);
-    }
+//    public Item getItemDropped(int p_149650_1_, Random p_149650_2_, int p_149650_3_)
+//    {
+//        return Item.getItemFromBlock(CommonProxy.machineScientificGrinder);
+//    }
 
     public void onBlockAdded(World p_149726_1_, int p_149726_2_, int p_149726_3_, int p_149726_4_)
     {
@@ -185,48 +185,48 @@ public class MachineScientificGrinder extends BlockContainer {
         }
     }
 
-    public void breakBlock(World p_149749_1_, int p_149749_2_, int p_149749_3_, int p_149749_4_, Block p_149749_5_, int p_149749_6_) {
-        if (!field_149934_M) {
-            TileEntityFurnace tileentityfurnace = (TileEntityFurnace)p_149749_1_.getTileEntity(p_149749_2_, p_149749_3_, p_149749_4_);
-
-            if (tileentityfurnace != null) {
-                for (int i1 = 0; i1 < tileentityfurnace.getSizeInventory(); ++i1) {
-                    ItemStack itemstack = tileentityfurnace.getStackInSlot(i1);
-
-                    if (itemstack != null) {
-                        float f = this.field_149933_a.nextFloat() * 0.8F + 0.1F;
-                        float f1 = this.field_149933_a.nextFloat() * 0.8F + 0.1F;
-                        float f2 = this.field_149933_a.nextFloat() * 0.8F + 0.1F;
-
-                        while (itemstack.stackSize > 0) {
-                            int j1 = this.field_149933_a.nextInt(21) + 10;
-
-                            if (j1 > itemstack.stackSize) {
-                                j1 = itemstack.stackSize;
-                            }
-
-                            itemstack.stackSize -= j1;
-                            EntityItem entityitem = new EntityItem(p_149749_1_, (double)((float)p_149749_2_ + f), (double)((float)p_149749_3_ + f1), (double)((float)p_149749_4_ + f2), new ItemStack(itemstack.getItem(), j1, itemstack.getItemDamage()));
-
-                            if (itemstack.hasTagCompound()) {
-                                entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
-                            }
-
-                            float f3 = 0.05F;
-                            entityitem.motionX = (double)((float)this.field_149933_a.nextGaussian() * f3);
-                            entityitem.motionY = (double)((float)this.field_149933_a.nextGaussian() * f3 + 0.2F);
-                            entityitem.motionZ = (double)((float)this.field_149933_a.nextGaussian() * f3);
-                            p_149749_1_.spawnEntityInWorld(entityitem);
-                        }
-                    }
-                }
-
-                p_149749_1_.func_147453_f(p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_);
-            }
-        }
-
-        super.breakBlock(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_, p_149749_6_);
-    }
+//    public void breakBlock(World p_149749_1_, int p_149749_2_, int p_149749_3_, int p_149749_4_, Block p_149749_5_, int p_149749_6_) {
+//        if (!field_149934_M) {
+//            TileEntityFurnace tileentityfurnace = (TileEntityFurnace)p_149749_1_.getTileEntity(p_149749_2_, p_149749_3_, p_149749_4_);
+//
+//            if (tileentityfurnace != null) {
+//                for (int i1 = 0; i1 < tileentityfurnace.getSizeInventory(); ++i1) {
+//                    ItemStack itemstack = tileentityfurnace.getStackInSlot(i1);
+//
+//                    if (itemstack != null) {
+//                        float f = this.field_149933_a.nextFloat() * 0.8F + 0.1F;
+//                        float f1 = this.field_149933_a.nextFloat() * 0.8F + 0.1F;
+//                        float f2 = this.field_149933_a.nextFloat() * 0.8F + 0.1F;
+//
+//                        while (itemstack.stackSize > 0) {
+//                            int j1 = this.field_149933_a.nextInt(21) + 10;
+//
+//                            if (j1 > itemstack.stackSize) {
+//                                j1 = itemstack.stackSize;
+//                            }
+//
+//                            itemstack.stackSize -= j1;
+//                            EntityItem entityitem = new EntityItem(p_149749_1_, (double)((float)p_149749_2_ + f), (double)((float)p_149749_3_ + f1), (double)((float)p_149749_4_ + f2), new ItemStack(itemstack.getItem(), j1, itemstack.getItemDamage()));
+//
+//                            if (itemstack.hasTagCompound()) {
+//                                entityitem.getEntityItem().setTagCompound((NBTTagCompound)itemstack.getTagCompound().copy());
+//                            }
+//
+//                            float f3 = 0.05F;
+//                            entityitem.motionX = (double)((float)this.field_149933_a.nextGaussian() * f3);
+//                            entityitem.motionY = (double)((float)this.field_149933_a.nextGaussian() * f3 + 0.2F);
+//                            entityitem.motionZ = (double)((float)this.field_149933_a.nextGaussian() * f3);
+//                            p_149749_1_.spawnEntityInWorld(entityitem);
+//                        }
+//                    }
+//                }
+//
+//                p_149749_1_.func_147453_f(p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_);
+//            }
+//        }
+//
+//        super.breakBlock(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_, p_149749_6_);
+//    }
 
     /**
      * A randomly called display update to be able to add particles or other items for display
---
+++ src/main/java/com/labswag/seamusfd/client/render/RenderScientificGrinder.java
@@ -0,0 +1,8 @@
+package com.labswag.seamusfd.client.render;
+
+/**
+ * Created by SeamusFD on 6/23/14.
+ * All rights belong to me!!
+ */
+public class RenderScientificGrinder {
+}
